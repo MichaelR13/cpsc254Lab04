4995a0102a4b0bb6e70ee8a863d9b90982ecff88
ShawnMelwani <78571312+ShawnMelwani@users.noreply.github.com>
2023-02-16 13:43:46 -0800

    Update SyntaxAnalyzer.h

--- SyntaxAnalyzer.h
+++ SyntaxAnalyzer.h
@@ -1,1 +1,14 @@
+/**
+* Original Author: Shawn Melwani
+* Created: 2023/02/16
+* Last Updated by: Add your name here
+* Contact information: shawnmelwani@csu.fullerton.edu
+* Purpose of this file
+* Take in a vector token stream and sorts it into a Syntax Tree and than outputs that syntax tree
+* (c) Copyright/license info
+**/
+
+
+#ifndef SyntaxAnalyzer
+#define SyntaxAnalyzer
 
