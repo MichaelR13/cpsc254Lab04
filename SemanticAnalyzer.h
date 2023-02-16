b5b8b3c450d4364ef8455315ef6627cba52a8fda
fllamas <chewfasa5@hotmail.com>
2023-02-16 13:46:27 -0800

    Added comment.

---
+++ SemanticAnalyzer.h
@@ -0,0 +1,10 @@
+/**
+* Original Author: Alfredo Llamas
+* Created: 2023/02/16   
+* Last Updated by: Alfredo Llamas
+* Contact information: frankllamas@csu.fullerton.edu
+* Purpose of this file
+* Description of Classes, Functions, etc. in the file.
+* (c) Copyright/license info
+**/
+
