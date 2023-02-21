8ab66d60f290e06f0a8623556fd123d3418a7431
Elexzandreia <54645199+Elexzandreia@users.noreply.github.com>
2023-02-21 12:32:43 -0800

    Created LexicalAnalyzer.h and filled in basic info

---
+++ LexicalAnalyzer.h
@@ -0,0 +1,11 @@
+/*
+ Original Author: Elexzandreia Livermore
+ Created: 2023/02/21   
+ Last Updated by: Elexzandreia
+ Contact information: elexzandreia@csu.fullerton.edu
+ Purpose of this file:
+ The purpose of this file is to intake source code with a class and output a vector token stream using the function "void read_input()"
+ Description of Classes, Functions, etc. in the file:
+ The function will be called read_input() and will make a vector token stream. This is all for Phase 1 (referring to our chart/diagram)
+ (c) Copyright/license info
+*/
